8e35483ed9e0478924af8bd9515e716d49cdab21
angelcrimson <a.rodriguez@crimsoneducation.org>
2026-01-12 17:24:24 -0800

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,33 @@
+print("Hello world!")
+
+# This is how you write comments in python
+
+#This is known as a string
+person1 = "angel"
+person2 = "sean"
+
+# This is a list
+l1 = [1,2,3,4,5]
+# it holds objects
+
+l1.append(6)
+print(l1)
+l1.append("banana")
+print(l1)
+print(len(l1))
+
+print(person1)
+
+print(person1 + " is " + person2 + "`s mentor!")
+
+# You can do math on python
+print(2+4)
+
+
+# Python has a lot of really cool libraries that you can use for anything that you can imagine
+#math is a library of math functions, so you can do advanced math operations
+import math 
+
+
+print(math.sqrt(16))
+
